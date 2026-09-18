ff107706dcd078d68ec8fe350ea684480cbf16b8
Yevhenii Naumenko <zenanaumenko@gmail.com>
2026-09-18 22:51:46 +0300

    Updates

--- _next/static/nWnFWiHkN9c15qFParccB/_buildManifest.js
+++ _next/static/nWnFWiHkN9c15qFParccB/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Kpi//_next/:path+",
+        "destination": "/Kpi/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
